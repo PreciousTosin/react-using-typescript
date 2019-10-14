2ebbb7457f7f8ff82593cc79c74c2800ae4e11c3
Akinbobola Tosin <precioustosin@hotmail.com>
2019-10-14 21:04:20 +0100

    animate components

--- src/components/tx/components/progressbar/index.tsx
+++ src/components/tx/components/progressbar/index.tsx
@@ -1,32 +1,67 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent, useState, useEffect, useRef } from "react";
 
 import "./index.scss";
 
-const ProgressBar: FunctionComponent = props => {
+interface ProgressBarProps {}
+
+const ProgressBar = (props: ProgressBarProps) => {
+  const { start, stop, stopAnimation } = props;
   const [width, setWidth] = useState(1);
 
-  let animation;
-  let progress = 1;
+  const animationRef = useRef<number>();
+  const maxValueRef = useRef(100);
+  const maxFPSRef = useRef(60);
+  const frameCountRef = useRef(0);
+  const frameRateRef = useRef(30);
+  let increment = 1;
+  let progress = 0;
+  let timeStamp;
 
   useEffect(() => {
     animateBar();
+    // if (start) animateBar();
     // startAnimation(frame);
+    // animationRef.current = requestAnimationFrame(animateBar);
+    return () => cancelAnimationFrame(animationRef.current);
   }, []);
 
   function frame() {
-    // console.log("CALLED FRAME");
-    if (progress >= 100) {
-      cancelAnimationFrame(animation);
+    let difference = Date.now();
+    if (timeStamp !== null) difference = Date.now() - timeStamp;
+    // if (difference > 20) return;
+    console.log("Time", difference);
+    // console.log("CALLED FRAME PROGRESS", progress, width);
+    console.log("CANCELLING", progress > 100);
+    if (progress > 100) {
+      // console.log('CANCELLING', progress <= maxValueRef.current);
+      cancelAnimationFrame(animationRef.current);
+      return;
     } else {
-      progress = progress + 3;
+      progress = progress + increment;
       console.log("CALLED FRAME", progress);
-      setWidth(prevState => prevState + 3);
+      // console.log("FPS", frameCountRef.current, maxFPSRef.current, frameRateRef.current);
+
+      /* frameCountRef.current++;
+      if (frameCountRef.current >= Math.round(
+        maxFPSRef.current / frameRateRef.current
+    )) {
+      setWidth(prevState => prevState + increment);
+      frameCountRef.current = 0;
+    } */
+      // if (difference < 20) setWidth(prevState => prevState + increment);
+      // if (difference < 20 || progress >= maxValueRef.current)
+      // setWidth(progress);
+      setWidth(prevState => prevState + increment);
+      timeStamp = Date.now();
     }
   }
 
   function animateBar() {
     frame();
-    animation = requestAnimationFrame(animateBar);
+    // console.log('ANIMATION: ', animationRef);
+    if (progress < maxValueRef.current)
+      animationRef.current = requestAnimationFrame(animateBar);
+    // console.log('ANIMATION: ', animationRef);
   }
 
   return (
--- src/components/tx/index.tsx
+++ src/components/tx/index.tsx
@@ -1,24 +1,32 @@
-import React, { FunctionComponent, useEffect, useRef, forwardRef } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 
 import ProgressBar from "./components/progressbar";
 
 import "./index.scss";
 
-const Bar = forwardRef((props, ref) => <ProgressBar ref={ref} />);
+// const Bar = forwardRef((props, ref) => <ProgressBar ref={ref} />);
 
 const Tx: FunctionComponent = props => {
+  const [animate, setAnimation] = useState(false);
   const progressbar = useRef<Element>(null);
 
-  function animate() {
+  function animateBar() {
     console.log("PROGRESS", progressbar);
+    setAnimation(true);
   }
+
+  function stopAnimation() {
+    setAnimation(false);
+  }
+
   return (
     <div className="tx-container">
       <h4>TXS</h4>
+      <div>{animate && <ProgressBar />}</div>
+      <button onClick={animateBar}>ANIMATE</button>
       <div>
-        <Bar />
+        <button onClick={stopAnimation}>REMOVE ANIMATION</button>
       </div>
-      <button onClick={animate}>ANIMATE</button>
     </div>
   );
 };
